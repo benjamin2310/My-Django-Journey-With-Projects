60d601a7645eba5254020040ab2bc730a8cb1075
Benjamin Kwame Ampah <ampahben3@gmail.com>
2025-01-26 00:15:14 +0000

    created models

--- Chapter4-Message-Board/posts/models.py
+++ Chapter4-Message-Board/posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    text = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.text[:250]
+
+class Author(models.Model):
+    author_name = models.TextField()
+    author_email = models.EmailField()
+    author_date = models.DateField()
+
+    def __str__(self):
+        return self.author_name[:50]
+
+class Student(models.Model):
+    name = models.TextField(max_length=200)
+    school = models.TextField(max_length=200)
+    student_email = models.EmailField()
+    student_programme = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.student_programme[:50]
+
