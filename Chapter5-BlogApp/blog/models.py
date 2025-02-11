895e340892cff5d0f008ab6191f88f2c992dbb19
Benjamin Kwame Ampah <ampahben3@gmail.com>
2025-02-11 17:06:51 +0000

    database design

--- Chapter5-BlogApp/blog/models.py
+++ Chapter5-BlogApp/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    description = models.TextField()
+    date_of_post = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={"pk": self.pk})  
